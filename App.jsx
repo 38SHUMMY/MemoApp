440c82bb75f9167c53385220ac5fc60910f7583c
shutobi <67335921+38SHUMMY@users.noreply.github.com>
2021-03-13 05:22:23 +0900

    Create MemoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    // <MemoListScreen />
     // <MemoDetailScreen />
+    // <MemoEditScreen />
+    <MemoCreateScreen />
   );
 }
